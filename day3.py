499f9cbbcaf696240aef84a542186426df1a67f9
Pranav9978 <khegadepanu@gmail.com>
2025-08-17 21:24:26 +0530

    Create day3.py
    day 3 challenge

---
+++ day3.py
@@ -0,0 +1,18 @@
+def findDuplicateValue (nums):
+    
+    slow, fast = nums[0], nums[0]
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+        if slow == fast:
+            break
+    
+
+    slow = nums[0]
+    while slow != fast:
+        slow = nums[slow]
+        fast = nums[fast]
+    
+    return slow
+    
+print(findDuplicateValue ([ 1, 3, 4, 2,5,2]))
